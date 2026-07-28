5cc6016a374c8e62cdfb6af32ef8a83513d82ef2
rishu <rrishu212@gmail.com>
2026-07-28 19:44:32 +0530

    Backend Order Services

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-import Product from "../model/Product.js";
+import Product from "../models/Product.js";
 import cloudinary from "../config/cloudinary.js";
 
 export const getProducts = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-// app.use("/api/orders", orderRoutes);
+app.use("/api/orders", orderRoutes);
 // app.use("/api/payments", paymentRoutes);
 // app.use("/api/analytics", analyticsRoutes);
 
