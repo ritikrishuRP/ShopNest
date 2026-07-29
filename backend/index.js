17f1d3fcce4e0a81c8ea827ec0dfb05b51f4a65b
rishu <rrishu212@gmail.com>
2026-07-30 02:18:28 +0530

    Final Deployment Config

--- backend/index.js
+++ backend/index.js
@@ -2,31 +2,35 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import authRoutes from "./routes/authRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
-import paymentRoutes from "./routes/paymentRoutes.js"
-import adminRoutes from "./routes/adminRoutes.js"
-import analyticsRoutes from "./routes/analyticsRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
 
 dotenv.config();
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: ['http://localhost:9000', 'http://127.0.0.1:9000', process.env.FRONTEND_URL],
-  credentials: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ],
+    credentials: true,
+  })
+);
 
-app.get("/", (req, res) => {
-    res.send("ShopNest Backend is working properly")
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
@@ -34,22 +38,20 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-//serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 } else {
-  app.get('/', (req, res) => {
-    res.send('ShopNest API is running in Development mode...');
+  app.get("/", (req, res) => {
+    res.send("ShopNest API running...");
   });
 }
 
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
