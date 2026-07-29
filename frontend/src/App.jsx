262126a7f6075b7828160f483156e7a0d7c96bbd
rishu <rrishu212@gmail.com>
2026-07-29 13:58:07 +0530

    Basic Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
